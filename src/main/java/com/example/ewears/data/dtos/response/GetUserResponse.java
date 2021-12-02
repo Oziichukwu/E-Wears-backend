637c97b077d5cb3f137368e606864b65c89e3365
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-02 19:59:23 +0100

    updated the get user response dto

--- src/main/java/com/example/ewears/data/dtos/response/GetUserResponse.java
+++ src/main/java/com/example/ewears/data/dtos/response/GetUserResponse.java
@@ -1,10 +1,27 @@
 package com.example.ewears.data.dtos.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class GetUserResponse {
 
     private String userId;
 
     private String userName;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
 
 
 }
