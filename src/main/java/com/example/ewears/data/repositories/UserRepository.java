fc1a2b24eafd1a9f52533f5b553e22801da699e2
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-03 05:01:12 +0100

    updated the user repository

--- src/main/java/com/example/ewears/data/repositories/UserRepository.java
+++ src/main/java/com/example/ewears/data/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.ewears.data.repositories;
 
 import com.example.ewears.data.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByUserName(String userName);
