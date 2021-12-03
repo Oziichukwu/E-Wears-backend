792e8b00a6eba3e424d8e33d7d4d3d368c463d78
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-03 06:06:28 +0100

    model role added and updated

--- src/main/java/com/example/ewears/data/models/Role.java
+++ src/main/java/com/example/ewears/data/models/Role.java
@@ -1,2 +1,46 @@
-package com.example.ewears.data.models;public class Role {
+package com.example.ewears.data.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ROLE")
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "ROLE_ID", updatable = false, nullable = false)
+    private String id;
+
+    @Column(name = "ROLE_NAME", nullable = false)
+    private String roleName;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+        cascade = CascadeType.ALL,
+        mappedBy = "roles")
+    @JsonIgnore
+    private Set<User> users = new HashSet<>();
+
+    @Column(name = "ROLE_DESCRIPTION", nullable = false)
+    private String roleDescription;
+
+    public void addUser(User user){
+        this.users.add(user);
+
+    }
+
 }
