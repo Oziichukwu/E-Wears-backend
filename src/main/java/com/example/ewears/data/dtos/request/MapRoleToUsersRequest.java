a8d69e8a16119ca3a980eb30f8baa74ce85db462
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-03 07:08:15 +0100

    dto created

--- src/main/java/com/example/ewears/data/dtos/request/MapRoleToUsersRequest.java
+++ src/main/java/com/example/ewears/data/dtos/request/MapRoleToUsersRequest.java
@@ -1,2 +1,15 @@
-package com.example.ewears.data.dtos.request;public class MapRoleToUserRequest {
+package com.example.ewears.data.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MapRoleToUsersRequest {
+
+    private List<String> userNames;
 }
