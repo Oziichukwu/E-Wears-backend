e27482262e4f4e3209393ace8e585f7cc3501be5
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-03 05:02:19 +0100

    added exception class

--- src/main/java/com/example/ewears/exceptions/E_WearsException.java
+++ src/main/java/com/example/ewears/exceptions/E_WearsException.java
@@ -1,2 +1,8 @@
-package com.example.ewears.exceptions;public class E_WearsException {
+package com.example.ewears.exceptions;
+
+public class E_WearsException extends RuntimeException{
+
+    public E_WearsException(String message) {
+        super(message);
+    }
 }
