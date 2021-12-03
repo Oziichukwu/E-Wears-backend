10a4a1c81cb38d8de86f3c2d43252fe319a2da2e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-03 06:05:22 +0100

    exception error added

--- src/main/java/com/example/ewears/exceptions/ErrorResponse.java
+++ src/main/java/com/example/ewears/exceptions/ErrorResponse.java
@@ -1,2 +1,20 @@
-package com.example.ewears.exceptions;public class ErrorResponse {
+package com.example.ewears.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+
+    private UUID uuid;
+    private List<Error> errors;
+
 }
