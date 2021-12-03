54f061a72ed7ecb886965b9d82370a2365063a0a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-03 16:56:05 +0100

    exception added

--- src/main/java/com/example/ewears/exceptions/SuccessCodeWithErrorResponseException.java
+++ src/main/java/com/example/ewears/exceptions/SuccessCodeWithErrorResponseException.java
@@ -1,2 +1,20 @@
-package com.example.ewears.exceptions;public class SuccessCodeWithErrorResponseException {
+package com.example.ewears.exceptions;
+
+import lombok.Getter;
+
+public class SuccessCodeWithErrorResponseException extends RuntimeException {
+
+    @Getter
+    private ErrorResponse errorResponse;
+
+    @Getter
+    private String userId;
+
+    public SuccessCodeWithErrorResponseException(String userId, ErrorResponse errorResponse) {
+
+        this.userId = userId;
+
+        this.errorResponse = errorResponse;
+    }
+
 }
