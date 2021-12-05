1d355e8c5545038a696d65ac5fcd090ebd05435d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-05 19:22:26 +0100

    gender added

--- src/main/java/com/example/ewears/data/models/Gender.java
+++ src/main/java/com/example/ewears/data/models/Gender.java
@@ -3,4 +3,17 @@ package com.example.ewears.data.models;
 public enum Gender {
     MALE,
     FEMALE;
+
+    @Override
+    public String toString(){
+
+        String value = "";
+        switch (this){
+            case MALE -> value = "Male";
+
+            case FEMALE -> value = "Female";
+        }
+        return value;
+
+    }
 }
