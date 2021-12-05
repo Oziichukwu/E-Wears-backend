feabbe23df6a3fd45070632d88d3dca384f27f19
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-05 21:35:54 +0100

    role service updated

--- src/main/java/com/example/ewears/services/RoleService.java
+++ src/main/java/com/example/ewears/services/RoleService.java
@@ -1,8 +1,13 @@
 package com.example.ewears.services;
 
 import com.example.ewears.data.dtos.request.CreateRoleRequest;
+import com.example.ewears.data.models.Role;
+
+import java.util.List;
 
 public interface RoleService {
 
     String createRole(CreateRoleRequest createRoleRequest);
+
+    List<Role> findAllRoles();
 }
