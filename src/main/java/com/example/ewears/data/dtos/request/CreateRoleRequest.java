6af9481a30998139c0e9e06b8c40bfe740af3adc
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-05 20:03:39 +0100

    create role request dto

--- src/main/java/com/example/ewears/data/dtos/request/CreateRoleRequest.java
+++ src/main/java/com/example/ewears/data/dtos/request/CreateRoleRequest.java
@@ -1,2 +1,22 @@
-package com.example.ewears.data.dtos.request;public class createRoleRequest {
+package com.example.ewears.data.dtos.request;
+
+
+import com.example.ewears.data.models.DateAudit;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateRoleRequest extends DateAudit {
+
+    @NotBlank
+    private String roleName;
+
+    @NotBlank
+    private String roleDescription;
+
 }
